638abb40b78ea57bc6c666cb30bd8b5c7ed8f91a
EC2 Default User <ec2-user@ip-172-31-64-179.ec2.internal>
2020-10-10 05:45:03 +0000

    add button to hello.html that has an alert js message. add function to hello.js onclick

--- static/hello.js
+++ static/hello.js
@@ -1,2 +1,8 @@
 // Everything in between these tags is Javascript!
 alert("Hello, world!");
+
+window.onload=function(){
+    document.getElementById("awesome_button").onclick = function(){
+        alert("I clicked the button!");
+    }
+}
